0c4e7573c99f6f1009a77d86a23a58be341302b9
Brad-is-Love <bradsandilands@hotmail.com>
2021-10-16 17:27:14 +1300

    Styling on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,31 +9,39 @@ function Home() {
 
     return (
         <div>
-        <div class="row">
+        <div class="container">
             
             <p class="specialMobile d-lg-none d-xl-none d-md-none">The only cryptocurrency exclusively focussed on bringing the Good Times.</p>
             <p class="special d-none d-lg-block d-xl-block d-md-block">The only cryptocurrency exclusively focussed on bringing the Good Times.</p>
+            
             <br />
-            <div class="col px-3">
-            <ol> <p>
-                <br />
-                <li><strong>Planting the good times:</strong> <br />
-                    The first step is to give 68GTC to 1000 Good Timers, via Reddit<br />
-                </li>
-                <br /> <br />
-                <li><strong>Growing the good times:</strong> <br />
-                    <ul>
-                        <li>Check out the Life Advice NFT generator below, if you need a little help getting the good times rolling.</li>
-                        <li>The Good Times Coin NFT collection is coming soon. Check it out below, get your GTC and get ready for the airdrop!</li>
-                    </ul>
-                    </li>
+            <hr />
+            <p class="text-center px-5" >
+                Get your&nbsp;<strong>free 68 GTC</strong> by messaging&nbsp;<a href="https://www.reddit.com/user/GoodTimesBradTimes" target="_blank" rel="noopener noreferrer">GoodTimesBradTimes</a> on reddit and get the good times rollin! <br />
+                *your reddit account must've been created before September 18
             </p>
-            </ol>
-            <br />
-            <p>Get your 68 GTC by messaging u/goodtimesbradtimes on reddit (account must've been created before September 18) and get the good times rollin'.</p>
+            <hr /><br />
+            
+                <div class="row justify-content-center">
+                    <div class="col-lg col-md-12 border rounded m-3 p-3">
+                        <p class="text-justify">
+                            Good Times Coin is currently in the distribution phase. Where 68GTC is given to 1000 people via reddit. 
+                            Check out the&nbsp;<a href="https://drive.google.com/file/d/1mGGZ02D6ULzOyikbOTPXzgC7BgJXjwBm/view?usp=sharing" target="_blank" rel="noopener noreferrer">whitepaper</a> for more info.
+                        </p>
+                    </div>
+                    <div class="col-lg col-md-12 border rounded m-3 p-3">
+                        <p class="text-justify">
+                            If you need a little help getting the good times rolling, check out our <a href="/life-advice">Life Advice NFT generator.</a>
+                        </p>
+                    </div>
+                    <div class="col-lg col-md-12 border rounded m-3 p-3">
+                        <p class="text-justify">
+                        The Good Times Coin NFT collection is coming soon. have a look at <a href="/good-nf-timers">Good NF Timers</a> and get ready for the airdrop!
+                        </p>
+                    </div>
             </div>
-           
     </div>
+    <br />
     <LifeAdvice/>
     <Sample/>
     <Roadmap/>
