86d0015acd77178557d8bdc48b5542f76c2dd55e
Brad-is-Love <bradsandilands@hotmail.com>
2021-12-28 09:47:13 +1300

    layout fix

--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -5,14 +5,14 @@ export default function CustomButton(props) {
 
     if(props.account!=="Not Connected"){
         return (
-                <button class="btn btn-large shadow-sm buttonText mx-3"  onClick={(event)=>{
+                <button class="btn btn-large shadow-sm buttonText m-3"  onClick={(event)=>{
                         event.preventDefault()
                         props.buttonFunction()
                     }}>{props.buttonText}
                 </button>
     )}else{
         return(
-                <button class="shadow-sm buttonDisabled mx-3">Not Connected 
+                <button class="shadow-sm buttonDisabled m-3">Not Connected 
                 </button>
         )
     }
