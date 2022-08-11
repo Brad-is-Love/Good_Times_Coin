6c9090498cdc3b9214c6861e02cc6462c570b339
Brad <bradsandilands@hotmail.com>
2022-08-11 16:41:18 +0800

    fleshed out profile page

--- src/components/StoreComponents/Account.js
+++ src/components/StoreComponents/Account.js
@@ -1,11 +1,43 @@
 import React from 'react'
 
-export const Account = (props) => {
+const Account = (props) => {
+
+
+  if (props.account === "Not Connected"){
+    return(
+      <>
+        <div className='siteHeading'>Not Connected</div>
+        <div>Ideally could have a button here to connect</div>
+      </>
+    )
+  }
+
   return (
-    <div>
+    <>
         <div className='siteHeading'>Account</div>
         <div className='smallBlue'>{props.account}</div>
-    </div>
+        the info below should only display if there is any and should probably be from a component
+        <div className='special'>
+          Reading Profile
+        </div>
+        <div>
+          <strong>Books owned:</strong> <br/>
+          call Profile.booksOwned()<br/>
+          <strong>Reviews:</strong>
+        </div>
+
+        <div className='special'>
+          Author Profile
+        </div>
+        <div>
+          <strong>Books published:</strong> <br/>
+          call Profile.booksPublished()<br/>
+          <strong>Reviews:</strong><br/>
+          <strong>Money Earned</strong><br/>
+          <strong>Author rank?</strong>
+
+        </div>
+    </>
   )
 }
 
